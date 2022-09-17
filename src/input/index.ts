9a0bfa0588294d76346dbbdbf0e77bf4e5caa82e
qfourca <qfourca3305@gmail.com>
2022-09-17 17:07:46 +0900

    selfie

---
+++ src/input/index.ts
@@ -0,0 +1,12 @@
+export default class Input {
+    private checks:Map<string, (degrees: Map<string, number>) => boolean> = new Map()
+    constructor (
+
+    ) {
+
+    }
+    public do(degrees: Map<string, number>) {
+
+        // console.log(degrees)w
+    }
+}
