270f42166a8878bed1ee5e708ce1d0ade6261126
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-09-17 23:26:48 +0900

    check bend & do function one time

--- src/input/index.ts
+++ src/input/index.ts
@@ -1,16 +1,30 @@
-import eventListener from "../global/eventlistener"
+import eventListener from "../global/eventlistener";
 
 export default class Input {
-    private checks:Map<string, (degrees: Map<string, number>) => boolean> = new Map()
-    constructor (
+  private checks: Map<string, (degrees: Map<string, number>) => boolean> =
+    new Map();
 
-    ) {
+  private bend: boolean;
 
+  constructor() {
+    this.bend = false;
+  }
+  public do(degrees: Map<string, number>) {
+    // console.log(degrees)
+    let leftArm = degrees.get("leftArm");
+    if (leftArm != undefined){
+        if (leftArm > 80 && leftArm < 180){
+            if (this.bend){
+                this.bend = false
+                console.log("실행")
+                eventListener.execute("input-walk",1000)
+            }
+        } else if(leftArm < 80 && leftArm > 0) { // 굽혔을 때
+            this.bend = true
+        }
     }
-    public do(degrees: Map<string, number>) {
-        let temp = degrees.get("leftArm")
-        if(temp != undefined)
-            if(!(temp > 90 && temp < 180))
-                eventListener.execute('input-walk', 1000)
-    }
+
+    // undefined됐을 때 바꿔주기도,
+
+  }
 }
