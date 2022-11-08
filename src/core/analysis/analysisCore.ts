49353d570b8c51e17606991f0ca0d20f02115bb3
박병관 <85085375+codingbotPark@users.noreply.github.com>
2022-11-09 01:02:40 +0900

    [ADD] sideLateralRaiseAnalysis

--- src/core/analysis/analysisCore.ts
+++ src/core/analysis/analysisCore.ts
@@ -1,16 +1,21 @@
 import Analysis from "./analysis";
 import leftDumbbellCurlAnalysis from "./leftDumbbellCurlAnalysis";
 import rightDumbbellCurlAnalysis from "./rightDumbbellCurlAnalysis";
+import sideLateralRaiseAnalysis from "./sideLateralRaiseAnalysis";
 
 export default class AnalysisCore{
     public static leftDumbbellCurlId = "left-dumbbellCurl"
     public static rightDumbbellCurlId = "right-dumbbellCurl"
+    public static sideLateralRaiseAnalysis = "side-lateralRaise"
+
     private analysises:Map<string, Analysis> = new Map()
     constructor (
-
+        
     ) {
         this.analysises.set(AnalysisCore.leftDumbbellCurlId, new leftDumbbellCurlAnalysis())
         this.analysises.set(AnalysisCore.rightDumbbellCurlId, new rightDumbbellCurlAnalysis())
+        
+        this.analysises.set(AnalysisCore.sideLateralRaiseAnalysis, new sideLateralRaiseAnalysis())
     }
 
     public inputValue(joints: Map<string, number>) {
